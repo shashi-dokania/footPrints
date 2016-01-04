aa5fb92ce2954dc7031d2b304ac05e21c3ddb029
Shashi <shashi@Shashis-MacBook-Pro.local>
2016-01-04 10:46:21 -0800

    add config

---
+++ server/config/config.js
@@ -0,0 +1,12 @@
+/**
+ * A module that contains configuration details for cloudinary
+ *
+ */
+module.exports = {
+  cloud_name: 'terrifying-veg', 
+  api_key: '453558962138716', 
+  api_secret: 'q8WnadOGLWqVpJvxh8788YFW12o',
+  AWS_ACCESS_KEY_ID_AUDIO: 'AKIAIMKR2UHCM3SDOZEQ',
+  AWS_SECRET_ACCESS_KEY_AUDIO: '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',
+  S3_BUCKET: 'walking-tour-media'
+}
